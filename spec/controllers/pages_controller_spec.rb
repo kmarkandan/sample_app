666f4521ce6f98847fe59099db2c19567fa7b64a
Kartik Markandan <kmarkandan@shaw.ca>
2012-07-24 13:50:42 -0700

    Finished making changes to layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
     end
     it "should have the right title" do
        get "home"
-       response.should have_selector("title", :content=> "Ruby on Rails Tutorial Sample | Home")
+       response.should have_selector("title", :content=> "Ruby on Rails Tutorial Sample App| Home")
      end
   end
  
@@ -22,7 +22,7 @@ describe PagesController do
     end
     it "should have the right title" do
        get "contact"
-       response.should have_selector("title", :content=> "Ruby on Rails Tutorial Sample | Contact")
+       response.should have_selector("title", :content=> "Ruby on Rails Tutorial Sample App| Contact")
      end
   end 
   
@@ -33,7 +33,7 @@ describe PagesController do
      end
      it "should have the right title" do
         get "about"
-        response.should have_selector("title", :content=> "Ruby on Rails Tutorial Sample | About")
+        response.should have_selector("title", :content=> "Ruby on Rails Tutorial Sample App| About")
       end
    end 
  
