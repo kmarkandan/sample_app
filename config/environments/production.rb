aad75090118462296a122dad4333d7f65427c5ff
Kartik Markandan <kmarkandan@shaw.ca>
2012-07-26 19:42:12 -0700

    Just Trying got rid of precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile  += %w( screen.css.scss print.css.scss mine.css.scss custom.css )
-   config.assets.precompile += %w(screen.css.scss print.css.scss mine.css.scss custom.css )
+  # config.assets.precompile += %w(screen.css.scss print.css.scss mine.css.scss custom.css )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
